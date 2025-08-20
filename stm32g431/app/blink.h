32411a1c92b3daaf71317426c148414c8b1355cf
agvolodin <agvolodin@stdu.info>
2025-08-20 07:51:30 +0400

    make blink led projects for stm32g431 /MCU/feature-HW001

---
+++ stm32g431/app/blink.h
@@ -0,0 +1,15 @@
+/*
+ * blink.h
+ *
+ *  Created on: Aug 20, 2025
+ *      Author: aleksey
+ */
+
+#ifndef BLINK_H_
+#define BLINK_H_
+
+#include "main.h"
+void setup();
+void loop();
+
+#endif /* BLINK_H_ */
