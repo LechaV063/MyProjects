34a632c49b025f7c3bd7a52265863f00193b2c3c
agvolodin <agvolodin@stdu.info>
2025-08-25 07:17:02 +0400

    add comment in seg7.c for project stm32g431 branch HW002

--- stm32g431/app/seg7.c
+++ stm32g431/app/seg7.c
@@ -8,7 +8,7 @@
 #include <seg7.h>
 uint8_t digit = 0;
 extern uint32_t myTick;
-//7seg знакогенератор pgfedcba
+//7seg знакогенератор 
 uint16_t digits[10] = {
                     (A_Pin|B_Pin|C_Pin|D_Pin|E_Pin|F_Pin), 			// "0"
 					(B_Pin|C_Pin), 									// "1"
@@ -22,7 +22,8 @@ uint16_t digits[10] = {
 					(A_Pin|B_Pin|C_Pin|D_Pin|F_Pin|G_Pin), 			// "9"
                   };
 
-void display_symbol(int symbol, int decimal_place)
+//  функция display_symbol - отображает цифру symbol в разряде decimal_place
+void display_symbol(int symbol, int decimal_place) 
 {
 	if (decimal_place == 1)
 	{
@@ -37,24 +38,26 @@ void display_symbol(int symbol, int decimal_place)
 	HAL_GPIO_WritePin(DispPort, digits[symbol], GPIO_PIN_SET);
 }
 
+// функция dark_all() - гасит все сегменты индикатора
 void dark_all()
 {
 	HAL_GPIO_WritePin(DispPort, _D1_Pin|_D2_Pin|_D3_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(DispPort, digits[8], GPIO_PIN_RESET);
 }
 
+//  функция display - отображает число value 
 void display(int value)
 {
 	  uint8_t digit[3] = {0};
 	  static int current_digit = 1;
-	  for (int i = 3; i >= 1; i--)
+	  for (int i = 2; i > 0; i--)
 		  {
 			digit[i] = value % 10;
 			value = value / 10;
 		  }
 		if(HAL_GetTick() - myTick < LIGHTTIME)
 		{
-			display_symbol(digit[current_digit], current_digit);
+			display_symbol(digit[current_digit - 1], current_digit);
 
 		} else {
 			myTick = HAL_GetTick();
